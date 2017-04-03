9b2d3b9d889cc83ea754f4f55a571a6d87b795f9
gooliath12 <jamesguo1112@sina.com>
2017-04-03 14:48:55 -0400

    Add google Login

--- application.py
+++ application.py
@@ -1,6 +1,32 @@
 import flask
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for, session
+from flask_oauth import OAuth
+
+GOOGLE_CLIENT_ID = '852263075688-3u85br5hvvk6ajvrafavv3lpupns3va7.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'l7a5ztJX5bW9iZBTq81GP-pg'
+REDIRECT_URI = '/oauth2callback'
+
+# SECRET_KEY = 'development key'
+DEBUG = True
+
 application = Flask(__name__)
+application.debug = DEBUG
+application.secret_key = SECRET_KEY
+oauth = OAuth()
+
+google = oauth.remote_app(
+	'google',
+	base_url='https://www.google.com/accounts/',
+	authorize_url='https://accounts.google.com/o/oauth2/auth',
+	request_token_url=None,
+	request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+		'response_type': 'code'},
+	access_token_url='https://accounts.google.com/o/oauth2/token',
+	access_token_method='POST',
+	access_token_params={'grant_type': 'authorization_code'},
+	consumer_key=GOOGLE_CLIENT_ID,
+	consumer_secret=GOOGLE_CLIENT_SECRET
+)
 
 
 @application.before_request
@@ -22,7 +48,46 @@ def teardown_request(exception):
 
 @application.route('/')
 def index():
-	return render_template('index.html')
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+
+    access_token = access_token[0]
+    from urllib2 import Request, urlopen, URLError
+
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError, e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        return res.read()
+
+    return res.read()
+
+
+@application.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+
+
+@application.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
 
 
 if __name__ == "__main__":
